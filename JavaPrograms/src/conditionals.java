5d085b8bc598a748115f76a7509257420441fc4c
Vansh Vora <vanshv1611@gmail.com>
2024-12-18 18:10:01 +0530

    Add files via upload

---
+++ JavaPrograms/src/conditionals.java
@@ -0,0 +1,12 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int age=9;
+        if(age>18)
+        {
+            System.out.println("You can drive");
+        }
+        else {
+            System.out.println("you can not drive");
+        }
+    }
+}
